bd02f422eb654a28258a4818a97292ddf79cd082
chuTuanNam <namchu2311@gmail.com>
2022-09-29 13:12:37 +0700

    add file dequi.cpp

---
+++ dequi.cpp
@@ -0,0 +1,7 @@
+/*
+Quay lui là phương pháp giống đệ qui, 
+từ 1 hàm A -------                            -----------
+           |     |  -> ta gọi hàm B trong nó  |         | 
+           |     |                            |         |
+           -------                            -----------
+thì nó sẽ thực hiện hàm B rồi quay trở lại hàm A và tiếp tục hàm A

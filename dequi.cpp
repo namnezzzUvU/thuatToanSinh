798b71c746a6a5b20aa1bd366857a578b4c78cc3
chuTuanNam <namchu2311@gmail.com>
2022-09-29 16:14:22 +0700

    update quayLuiNhiPhan file

--- dequi.cpp
+++ dequi.cpp
@@ -5,4 +5,16 @@ từ 1 hàm A -------                            -----------
            |     |                            |         |
            -------                            -----------
 thì nó sẽ thực hiện hàm B rồi quay trở lại hàm A và tiếp tục hàm A
+-quay lui(int j) {
+    for int (i=khả năng 1(của các phần tử trong cấu hình);i<cấu hình cuối;++i){
+        if(điều kiện) {
+            command;
+            if(j=n) {
+                int kq;
+            }
+            else 
+            quaylui(j+1);
+        }
+    }
+}
 */

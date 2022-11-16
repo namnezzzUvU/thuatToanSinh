778cedfc8b08f0133053789625769202c121f562
chuTuanNam <namchu2311@gmail.com>
2022-11-16 16:48:09 +0700

    update sinhHoanVi file

--- sinhHoanVi.cpp
+++ sinhHoanVi.cpp
@@ -1,3 +1,18 @@
+/* ví dụ, 
+2 7 6 9 8 5 4 3 1
+xét từ trái sang, tìm chỗ đầu tiên mà
+arr[i]<arr[i+1] : ở đây là 6<9,i=3<i=4
+đánh dấu arr[i]: ở đây là 6,i=3
+lại xét từ bên trái sang, tìm
+phần tử đầu tiên lớn hơn arr[i]
+đó, ở đây là 6. Ta tìm đc 8,
+đổi chỗ 6 với 8 cho nhau
+sau đó reverse từ i=3+1, ở đây đã là 8.
+đến hết. ta được dãy:
+2 7 8 1 3 4 5 6 9
+tiếp tục, 
+2 7 8 1 3 4 5 9 6
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int arr[100],n;
